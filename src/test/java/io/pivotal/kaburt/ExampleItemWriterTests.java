62ab235412f00a813fb8a84d627a54be74f1d37f
Takayuki Kaburagi <tkaburagi@pivotal.io>
2015-06-23 17:35:10 +0900

    initial commit

---
+++ src/test/java/io/pivotal/kaburt/ExampleItemWriterTests.java
@@ -0,0 +1,15 @@
+package io.pivotal.kaburt;
+
+import io.pivotal.kaburt.ExampleItemWriter;
+
+import junit.framework.TestCase;
+
+public class ExampleItemWriterTests extends TestCase {
+
+	private ExampleItemWriter writer = new ExampleItemWriter();
+	
+	public void testWrite() throws Exception {
+		writer.write(null); // nothing bad happens
+	}
+
+}
